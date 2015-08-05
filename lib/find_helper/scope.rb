3ce305f718183f8114c7101081825226c73d8413
panaoke <panaoke@seasonmall.com>
2015-08-05 11:57:08 +0800

    add markdown doc and repair some bug to use active record query

--- lib/find_helper/scope.rb
+++ lib/find_helper/scope.rb
@@ -2,9 +2,18 @@ module FindHelper
 
 	module Scope
 		def by_scopes(attrs, allow_blank = false)
+			case attrs
+				when Array
+					by_array_scopes(attrs, allow_blank = false)
+				when Hash, HashWithIndifferentAccess
+					by_hash_scopes(attrs, allow_blank = false)
+			end
+		end
+
+		def by_hash_scopes(attrs, allow_blank = false)
 			attrs = attrs.delete_if{|_, value| value.blank? } unless allow_blank
 
-			attrs.inject(self) do |parent_scope, (key, value)|
+			attrs.inject(self.where({})) do |parent_scope, (key, value)|
 				scope_name = key.to_sym
 				is_named_scope = self.respond_to?(scope_name)
 
@@ -16,6 +25,12 @@ module FindHelper
 
 					parent_scope.where(finder_class.new(self, key, value).to_finder)
 				end
+			end
+		end
+
+		def by_array_scopes(attrs, allow_blank = false)
+			attrs.compact.inject(self.where({})) do |result, attr|
+				result.by_hash_scopes(attr, allow_blank)
 			end
 		end
 
